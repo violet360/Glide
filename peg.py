0889daf859692c0c9f18e199e2cb7db026944c53
violet360 <46649871+violet360@users.noreply.github.com>
2020-07-15 08:17:15 +0530

    added comments

--- peg.py
+++ peg.py
@@ -4,8 +4,7 @@ class Peg():
         self.x = x
         self.y = y
         self.color = color
-        self.rect = (x,y)
-        # self.vel = 3
+        self.position = (x,y)
         self.radius = radius
         self.vx, self.vy = 1, 1
 
@@ -13,20 +12,20 @@ class Peg():
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius )
 
     def get_point(self):
-        return (self.x, self.y)
+        return self.position
 
     def move(self):
         # pos = pygame.mouse.get_pos()
-        if(self.y >= 480 or self.y <= 20):
-            self.vy = (-1)*self.vy
+        if(self.y >= 480 or self.y <= 20): #if gets at the lower wall (y of peg >=480) or upper wall( y of peg <= 20) 
+            self.vy = (-1)*self.vy # just reverses the direction of the vy
 
 
-        if(self.x >= 480 or self.x <= 20):
-            self.vx = (-1)*self.vx
+        if(self.x >= 480 or self.x <= 20): # if gets hit at right or the left wall respectively
+            self.vx = (-1)*self.vx # just reverses the direction of vx
 
         self.x += self.vx
         self.y += self.vy
         self.update()
 
     def update(self):
-        self.rect = (self.x, self.y)
+        self.position = (self.x, self.y)
