3e6cc7cef9f92d6d8c0d678f52fe78c45c1b138d
violet360 <46649871+violet360@users.noreply.github.com>
2020-07-15 08:06:17 +0530

    reduced code length and added comments

--- server1.py
+++ server1.py
@@ -4,32 +4,27 @@ from player import Player
 import pickle
 from peg import Peg
 
-# class peg():
-
-
 server = socket.gethostname()
 port = 5555
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 try:
     s.bind((server, port))
 except socket.error as e:
     str(e)
-
 s.listen(2)
 print("Waiting for a connections.............")
 
-peg = Peg(25,50, 20, (0,255,0))
-players = [Player(0,0, 40, (255,0,0)), Player(0,0, 40,(0,0,255))]
+
+peg = Peg(25,50, 20, (0,255,0)) #peg object
+players = [Player(0,0, 40, (255,0,0)), Player(0,0, 40,(0,0,255))] #[player1, player2]
 
 def threaded_client(conn, player):
     global peg
-    conn.send(pickle.dumps([players[player], peg]))
+    conn.send(pickle.dumps([players[player], peg])) #sends [respectve_player, peg] to the respctive player 
     reply = ""
     while True:
         try:
-            data = pickle.loads(conn.recv(2048))
+            data = pickle.loads(conn.recv(2048)) #receives [respective_player, peg]
             players[player] = data[0]
             peg = data[1]
 
@@ -42,13 +37,10 @@ def threaded_client(conn, player):
                 else:
                     reply = [players[1], peg]
 
-                # for i in enumerate(players)
-                
-
                 print("Received: ", data)
-                print("Sending : ", reply)
+                print("Sending : ", reply) #if the "data" variable is received from player1 then "reply" will be [player2, peg],.............. else it will be [player1, peg]
 
-            conn.sendall(pickle.dumps(reply))
+            conn.sendall(pickle.dumps(reply)) 
         except:
             break
 
@@ -59,11 +51,5 @@ currentPlayer = 0
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
-
-    # if(currentPlayer == 1):
-    #     peg = Peg(50,50, 20, (0,255,0))
-
-
-
-    start_new_thread(threaded_client, (conn, currentPlayer))
+    start_new_thread(threaded_client, (conn, currentPlayer)) #start a new_thread
     currentPlayer += 1
