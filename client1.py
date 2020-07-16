5ded6cd9cc648a05373d5f75a7ce543856c97ad5
violet360 <46649871+violet360@users.noreply.github.com>
2020-07-17 00:18:10 +0530

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,102 +1,76 @@
-import pygame
-import socket
-from player import Player
-import pickle
-import math
-import time
+import pygame,time,math
+import sys,os #using this module to debug
+from network import *
+from participant import *
+
+pygame.init()
+
+size           = width, height = 1100,650
+striker_radius = height//15
+peg_radius     = int(striker_radius//1.5)
+
+black          = (0,0,0) 
+white          = (255,255,255)
+blue           = (0,0,255)
+red            = (255,0,0)
+green          = (0,255,0)
+light_green    = (0,200,0)
+exit_game      = False
+mouse_pos      = (width- striker_radius,height//2)#initial position of striker
+game_display   = pygame.display.set_mode(size)
+pygame.display.set_caption("Air Hockey")
+fps = 60
+		
 
 
 
-width = 500
-height = 500
-win = pygame.display.set_mode((width, height)) #window intialized
-pygame.display.set_caption("Client") #Caption set
+def make_ground():
 
+	game_display.fill((0, 0, 0))
+	pygame.draw.line  (game_display,white, (width//2,0),(width//2,height)  ,3)
+	pygame.draw.line  (game_display,white, (width,200), (width,400)        ,20)
+	pygame.draw.line  (game_display,white, (0,200), (0,400)                ,20)
+	pygame.draw.rect  (game_display,red  , (0,0,width,height)              ,4)
+	pygame.draw.circle(game_display,white, (width//2,height//2),height//6  ,3)
 
-class Network:
-	def __init__(self): #constructor
-		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.server = socket.gethostname()
-		self.port = 5555
-		self.addr = (self.server, self.port)
-		self.LIST= self.connect() #self.LIST = [player1, peg]
-		self.p = self.LIST[0]
-		self.peg = self.LIST[1]
-
-	def getP(self):
-		return self.p
-
-	def getPeg(self):
-		return self.peg
-
-	def connect(self): 
-		try:
-			self.client.connect(self.addr) #connects with the server
-			return pickle.loads(self.client.recv(2048)) #receives a list of player1 object and peg from the server 
-		except:
-			pass
-
-	def send(self, data):
-		try:
-			self.client.send(pickle.dumps(data)) #sends the [player1, peg] to the server
-			return pickle.loads(self.client.recv(2048)) # receives [player2, peg]
-		except socket.error as e:
-			print(e)
-
-
-def round_off(x): #this function rounds of  decimal... if so if 0<x<1 the return 1 else if {x}>=0.5 return [x+1]  else return [x]
-	if 0.0<x<1.0:
-		return 1
-
-	y = x - int(x)
-
-	if(y>=(0.5)):
-		return int(x+1)
-
-	else:
-		return int(x)
-
-def rel_vec(peg, player):
-	return round_off((peg[0] - player[0])), round_off((peg[1] - player[1]))
-
-
-def redrawWindow(win,player, player2, peg):
-	win.fill((255,255,255))
-	player.draw(win)
+def redrawWindow(win,player1, player2 ,peg):
+	player1.draw(win)
 	player2.draw(win)
 	peg.draw(win)
 	pygame.display.update()
 
-
-
-def dist(x, y):
-	return math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)])) #distance function
-
-def main():
-	run = True
-	n = Network() #creates an instance of the network connection with server
-	p = n.getP() # p is the object of (player 1)
-	peg = n.getPeg() # object of the peg
+def game_loop(exit_game,mouse_pos):
+	n = Network()
+	player1 = n.getP()
+	peg = n.getPeg()
+	peg_x = width//2
+	peg_y = height//2
 	clock = pygame.time.Clock()
-
-	while run:
+	while not exit_game:
 		clock.tick(60)
-		player2_obj, peg_obj = n.send([p, peg]) #sends the list of object of the player1 to the server and gets back the object of player2
-		p2 = player2_obj #player 2 object
-		peg = peg_obj #peg object
+		player2, peg = n.send([player1, peg])
+		make_ground()
+
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				run = False
-				pygame.quit()
+				exit_game = True
+				quit()
+
+		player1.move()
+		peg.move()
+		# hit = if_hit(player1,peg)
+		# if hit is not False:
+		# 	print(hit)
+		# 	peg.vx, peg.vy = hit
+
+		# hit = if_hit(player2, peg)
+		# if hit is not False:
+		# 	print(hit, "p2")
+		# 	peg.vx, peg.vy = hit
+
+		pygame.draw.line(game_display,green,peg.get_position(),player1.get_position())
+		redrawWindow(game_display, player1, player2, peg)
 
 
-		rx, ry= rel_vec(peg.get_point(), p.get_point()) # [rx , ry] is the relative vector of the peg wrt the striker
-
-		print(rx, ry)
-
-		p.move()  #this handles the movement of player , so when you move the mouse the move() method gets the mouse position and postions the player on the mouse pointer 
-		peg.move() #this handles the movement of the peg , this includes the bouncing of the wall
-
-		redrawWindow(win, p, p2, peg) #after each change in the respective positions of player and peg the screen is updated....
-
-main()
+game_loop(exit_game,mouse_pos)
